d3779fd0caa7d14612d5a7c89669874f55bb1fba
Point Hua <huawenhaohhh@126.com>
2022-07-28 13:25:43 +0800

    update 2 script

--- docker_change_source.sh
+++ docker_change_source.sh
@@ -1,1 +1,4 @@
 sudo apt update && sudo apt upgrade
+sudo touch /etc/docker/deamon.json
+sudo sed -i 's/\[".*"\]/["https:\/\/registry.docker-cn.com"]/g' /etc/docker/daemon.json
+sudo service docker restart
--- dockerinstall_Home Assistant.sh
+++ dockerinstall_Home Assistant.sh
@@ -1,2 +1,4 @@
 sudo apt update && sudo apt upgrade
 sudo docker pull homeassistant/home-assistant
+mkdir ~/Home_Assistant
+docker run -d --name="home-assistants" -v /[~/Home_Assistant]:/config -p 8123:8123 homeassistant/home-assistant
